fb05c84808df5397dca41aabc57f529f69f65ef6
sgmiguel <miguelsgg12@gmail.com>
2024-04-08 22:47:41 +0200

    feat: add reponsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <main className="w-screen h-screen flex justify-center items-center">
-      <section className="flex flex-col justify-center items-center">
-        <h1 className="fade-in-heading text-slate-50 text-[4rem] font-bold">
+      <section className="flex flex-col justify-center items-center gap-4">
+        <h1 className="fade-in-heading text-slate-50 text-5xl sm:text-[4rem] font-bold text-center">
           Miguel Sepúlveda
         </h1>
-        <h2 className="fade-in-heading animation-delay text-slate-200 text-[2.5rem] font-bold">
+        <h2 className="fade-in-heading animation-delay text-slate-200 text-3xl sm:text-[2.5rem] font-bold text-center">
           Full Stack Developer
         </h2>
       </section>
